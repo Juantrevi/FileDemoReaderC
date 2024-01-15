02616771f2f1dd4d72cfa3ce0f060117503431f1
Juantrevi <juantrevi70@gmail.com>
2024-01-15 11:49:57 +0000

    Initial commit

---
+++ FileDemoReader/Program.cs
@@ -0,0 +1,22 @@
+﻿
+try
+{
+    const string fileName = @"C:\Users\juant\OneDrive\Escritorio\Dorset\7 - OOP\FileDemo\FileDemoReader\sample.txt";
+    var reader = new StreamReader(fileName);
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        Console.WriteLine(line);
+    }
+
+    reader.Close();
+    Console.ReadLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Failed:" + e.Message);
+}
+finally
+{
+    Console.WriteLine("Finally block");
+}
